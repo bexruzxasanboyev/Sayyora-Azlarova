799ea8b56f01b652824e127de10303ad2c7b28e4
Bexruz Xasanboyev <mayoq2024@gmail.com>
2026-04-23 20:37:24 +0500

    push

--- pay/js/pay.js
+++ pay/js/pay.js
@@ -140,9 +140,9 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 // 1) ONE place to edit tariffs + prices
 const TARIFFS = {
-    "STANDART": { uzs: 1297000, label: "1 297 000 SO'M" },
-    "PREMIUM": { uzs: 1497000, label: "1 497 000 SO'M" },
-    "VIP": { uzs: 2297000, label: "2 297 000 SO'M" },
+    "STANDART": { uzs: 1497000, label: "1 497 000 SO'M" },
+    "PREMIUM": { uzs: 1797000, label: "1 797 000 SO'M" },
+    "VIP": { uzs: 2497000, label: "2 497 000 SO'M" },
 };
 
 
